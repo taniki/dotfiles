57aec1946807f4911a91f66b92bded91ba192aa6
duong tam kien <tk@renard.ninja>
2023-01-23 22:04:50 +0100

    ajoute une config noice

---
+++ nvim/.config/nvim/after/plugin/noice.lua
@@ -0,0 +1,6 @@
+require("noice").setup({
+  presets = {
+    -- command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+  }
+})
