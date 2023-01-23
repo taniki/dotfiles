7b96d4d2255d511ac6f841ebc4ee1d779827e789
duong tam kien <tk@renard.ninja>
2023-01-23 21:17:15 +0100

    ajoute le support de la variante `machiato`

--- nvim/.config/nvim/after/plugin/feline.lua
+++ nvim/.config/nvim/after/plugin/feline.lua
@@ -4,4 +4,15 @@ require("feline").setup({
     components = ctp_feline.get(),
 })
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+        package.loaded["feline"] = nil
+        package.loaded["catppuccin.groups.integrations.feline"] = nil
+        require("feline").setup {
+            components = require("catppuccin.groups.integrations.feline").get(),
+        }
+    end,
+})
+
 require('feline').winbar.setup()
